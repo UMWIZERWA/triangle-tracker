adc20b7c8f0ae5e1884da63a3ab4c8801470f279
UMWIZERWA <agamithe8@gmail.com>
2018-12-14 13:31:15 +0200

    declare sum variables

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ function mytrigon(){
 
         parseInt(document.getElementById("flank1").value),
         parseInt(document.getElementById("flank2").value),
-        parseInt(document.getElementById("flank3").value),   
+        parseInt(document.getElementById("flank3").value)];
+
+     var flanksum=parseInt(flankt[0])+parseInt(flank[1]);   
 }
