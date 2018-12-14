cc885f7e08e86b5f57b792f731f1b74677f002bb
UMWIZERWA <agamithe8@gmail.com>
2018-12-14 13:26:40 +0200

    create variables

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function mytrigon(){
-    
+    var flank  =[
+
+        parseInt(document.getElementById("flank1").value),
+        parseInt(document.getElementById("flank2").value),
+        parseInt(document.getElementById("flank3").value),   
 }
