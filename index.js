7318fb469cdf171084b46cc092f91d3d2451f50c
UMWIZERWA <agamithe8@gmail.com>
2018-12-14 13:32:39 +0200

    add if condition

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ function mytrigon(){
         parseInt(document.getElementById("flank2").value),
         parseInt(document.getElementById("flank3").value)];
 
-     var flanksum=parseInt(flankt[0])+parseInt(flank[1]);   
+     var flanksum=parseInt(flankt[0])+parseInt(flank[1]);
+     if (flank[0]==flank[1]&& flank[1]==flank[2])
+{ 
 }
